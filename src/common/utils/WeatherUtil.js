4cd412c28e16c5b7368704cbc53a0d86430c69cc
TaeHun Kim <nomadhuns@amuz.co.kr>
2025-07-07 14:41:21 +0900

    Feat: 금일 자외선 지수 데이터 통신 및 바인딩

--- src/common/utils/WeatherUtil.js
+++ src/common/utils/WeatherUtil.js
@@ -8,13 +8,11 @@ export function getNextFourHoursWeather(data) {
     const codes = data.hourly.weathercode.slice(index, index+4);
     const times = data.hourly.time.slice(index, index+4);
 
-    const merged = times.map((time, idx) => ({
+    return times.map((time, idx) => ({
         time: dayjs(time).format('HH.mm'),
         weathercode: codes[idx],
         temp: temps[idx]
     }));
-
-    return merged;
 }
 
 export function getWeeklyWeather(data) {
@@ -43,4 +41,25 @@ export function getMinAndMaxTemperatureToday(data) {
         minTemperature: data.daily.temperature_2m_min[0],
         maxTemperature: data.daily.temperature_2m_max[0],
     };
+}
+
+export function getUvIndex(uv) {
+    const value = Math.floor(uv);
+
+    switch (value) {
+        case 0:
+        case 1:
+        case 2: return 'Low'
+        case 3:
+        case 4:
+        case 5: return 'Moderate'
+        case 6:
+        case 7: return 'High'
+        case 8:
+        case 9:
+        case 10: return 'Very High'
+        default:
+            if (value >= 11) return 'Dangerous';
+            return '-';
+    }
 }
