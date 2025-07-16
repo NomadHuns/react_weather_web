78f697154aa18ef283bb7b80dec8761cbb40ee52
nomadHuns <kimalss22@gmail.com>
2025-07-16 12:17:44 +0900

    Refactor: 텍스트 컴포넌트 리팩토링

--- src/common/components/CommonText.js
+++ src/common/components/CommonText.js
@@ -1,50 +1,116 @@
 import styled from "styled-components";
+import {theme} from "../theme";
 
 const StyledText = styled.div`
-    font-size: ${props => props.fontSize || '24px'};
-    font-family: ${props => props.fontFamily || `'Poppins', sans-serif`};
-    letter-spacing: ${props => props.letterSpacing || '0.47px'};
-    line-height: ${props => props.lineHeight || 'normal'};
-    font-weight: ${props => props.fontWeight || 'normal'};
-    color: ${props => props.fontColor || '#FFFFFF'};
-    margin: ${props => props.margin || '0'};
+    font-size: ${props => props.fontSize || theme.text.default.fontSize};
+    font-family: ${props => props.fontFamily || theme.text.default.fontFamily};
+    letter-spacing: ${props => props.letterSpacing || theme.text.default.letterSpacing};
+    line-height: ${props => props.lineHeight || theme.text.default.lineHeight};
+    font-weight: ${props => props.fontWeight || theme.text.default.fontWeight};
+    color: ${props => props.fontColor || theme.text.default.fontColor};
 `;
 
-export function CommonText({children, fontSize, fontWeight, letterSpacing, fontColor, lineHeight, fontFamily}) {
+/**
+ * 텍스트 컴포넌트를 반환합니다.
+ * @param {string} children - 텍스트
+ * @param {string} fontSize - 폰트 사이즈
+ * @param {string} fontFamily - 폰트명
+ * @param {string} letterSpacing - 글자간 간격
+ * @param {string} lineHeight - 글자 높이
+ * @param {string} fontWeight - 글자 굵기
+ * @param {string} fontColor - 글자색
+ */
+export function CommonText({children, fontSize, fontFamily, letterSpacing, lineHeight, fontWeight, fontColor}) {
     return (
         <StyledText
             fontSize={fontSize}
-            fontWeight={fontWeight}
-            letterSpacing={letterSpacing}
-            fontColor={fontColor}
-            lineHeight={lineHeight}
             fontFamily={fontFamily}
+            letterSpacing={letterSpacing}
+            lineHeight={lineHeight}
+            fontWeight={fontWeight}
+            fontColor={fontColor}
         >
             {children}
         </StyledText>
     );
 }
 
-export function TitleText({children, fontWeight, fontColor, lineHeight}) {
+export function TitleText({children, ...rest}) {
     return (
-        <CommonText fontSize={'64px'} letterSpacing={'-0.37px'} fontWeight={fontWeight || 'bold'} fontColor={fontColor} lineHeight={lineHeight}>{children}</CommonText>
+        <CommonText
+            fontSize={theme.text.title?.fontSize}
+            fontFamily={ theme.text.title?.fontFamily}
+            letterSpacing={theme.text.title?.letterSpacing}
+            lineHeight={theme.text.title?.lineHeight}
+            fontWeight={theme.text.title?.fontWeight}
+            color={theme.text.title?.color}
+            {...rest}
+        >
+            {children}
+        </CommonText>
     );
 }
 
-export function SubjectText({children, fontsize = '28px'}) {
+export function SubjectText({children, ...rest}) {
     return (
-        <CommonText fontWeight={'600'} fontSize={fontsize} fontFamily={`"Open Sans", sans-serif`}>{children}</CommonText>
+        <CommonText
+            fontSize={theme.text.subject.fontSize}
+            fontFamily={ theme.text.subject.fontFamily}
+            letterSpacing={theme.text.subject.letterSpacing}
+            lineHeight={theme.text.subject.lineHeight}
+            fontWeight={theme.text.subject.fontWeight}
+            color={theme.text.subject.color}
+            {...rest}
+        >
+            {children}
+        </CommonText>
     );
 }
 
-export function ContentText({children, fontWeight, fontFamily}) {
+export function ContentText({children, ...rest}) {
     return (
-        <CommonText fontWeight={fontWeight} fontSize={'20px'} fontFamily={fontFamily}>{children}</CommonText>
+        <CommonText
+            fontSize={theme.text.content?.fontSize}
+            fontFamily={ theme.text.content?.fontFamily}
+            letterSpacing={theme.text.content?.letterSpacing}
+            lineHeight={theme.text.content?.lineHeight}
+            fontWeight={theme.text.content?.fontWeight}
+            color={theme.text.content?.color}
+            {...rest}
+        >
+            {children}
+        </CommonText>
     );
 }
 
-export function DescriptionText({children, fontWeight}) {
+export function DescriptionText({children, ...rest}) {
     return (
-        <CommonText fontWeight={fontWeight} fontSize={'16px'} fontFamily={`"Open Sans", sans-serif`}>{children}</CommonText>
+        <CommonText
+            fontSize={theme.text.description.fontSize}
+            fontFamily={ theme.text.description.fontFamily}
+            letterSpacing={theme.text.description.letterSpacing}
+            lineHeight={theme.text.description.lineHeight}
+            fontWeight={theme.text.description.fontWeight}
+            color={theme.text.description.color}
+            {...rest}
+        >
+            {children}
+        </CommonText>
+    );
+}
+
+export function HintText({children, ...rest}) {
+    return (
+        <CommonText
+            fontSize={theme.text.hint.fontSize}
+            fontFamily={ theme.text.hint.fontFamily}
+            letterSpacing={theme.text.hint.letterSpacing}
+            lineHeight={theme.text.hint.lineHeight}
+            fontWeight={theme.text.hint.fontWeight}
+            color={theme.text.hint.color}
+            {...rest}
+        >
+            {children}
+        </CommonText>
     );
 }
--- src/common/theme.js
+++ src/common/theme.js
@@ -3,4 +3,34 @@ export const theme = {
         mobile: `(max-width: 768px)`,
         tablet: `(max-width: 1024px)`,
     },
+    text: {
+        default: {
+            fontSize: '24px',
+            fontFamily: `'Poppins', sans-serif`,
+            letterSpacing: '0.47px',
+            lineHeight: 'normal',
+            fontWeight: 'normal',
+            fontColor: '#FFFFFF',
+            margin: '0px',
+        },
+        title: {
+            fontSize: '64px',
+            letterSpacing: '-0.37px',
+            fontWeight: 'bold',
+        },
+        subject: {
+            fontSize: '28px',
+            fontFamily: `"Open Sans", sans-serif`,
+            fontWeight: '600',
+        },
+        content: {
+            fontSize: '20px',
+        },
+        description: {
+            fontSize: '16px',
+            fontFamily: `"Open Sans", sans-serif`,
+        },
+        hint: {
+        },
+    }
 };
